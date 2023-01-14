88ad01632c2a8cb95f48a5a028dac36463cebde2
SoumyadeepPal21 <soumyadeepbappipal@gmail.com>
2023-01-14 11:10:10 +0530

    11 part done

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,28 @@
+import { VariableBinding } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router'
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  menuType:String = 'default';
+  constructor(private route : Router) { }
 
   ngOnInit(): void {
+    menuType:
+    this.route.events.subscribe((result : any) => {
+      if (result.url) {
+        if (localStorage.getItem('seller') && result.url.includes('seller')) {
+          console.warn("in seller");
+          this.menuType = "seller";
+        } else {
+          console.warn("outside seller");
+          this.menuType = "default";
+        }
+      }
+    });
   }
 
 }
