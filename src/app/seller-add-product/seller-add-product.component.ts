243ff1b43626f22308077254f04816f0ade4db22
SoumyadeepPal21 <soumyadeepbappipal@gmail.com>
2023-01-14 15:33:42 +0530

    15

--- src/app/seller-add-product/seller-add-product.component.ts
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -5,22 +5,20 @@ import { ProductService } from '../services/product.service';
 @Component({
   selector: 'app-seller-add-product',
   templateUrl: './seller-add-product.component.html',
-  styleUrls: ['./seller-add-product.component.css']
+  styleUrls: ['./seller-add-product.component.css'],
 })
 export class SellerAddProductComponent implements OnInit {
-  addProductMessage:string|undefined;
-  constructor(private product : ProductService) { }
+  addProductMessage: string | undefined;
+  constructor(private product: ProductService) {}
 
-  ngOnInit(): void {
-  }
-  submitProduct(data : product) {
-    this.product.addProduct(data).subscribe((result)=>{
+  ngOnInit(): void {}
+  submitProduct(data: product) {
+    this.product.addProduct(data).subscribe((result) => {
       // console.warn(result);
       if (result) {
-        this.addProductMessage = "Product is successfully added";
+        this.addProductMessage = 'Product is successfully added';
       }
-      setTimeout(()=>this.addProductMessage=undefined, 3000);
+      setTimeout(() => (this.addProductMessage = undefined), 3000);
     });
   }
-
 }
