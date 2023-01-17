57f5662b55232f1723ab50c84da91e30e410239d
SoumyadeepPal21 <soumyadeepbappipal@gmail.com>
2023-01-17 22:57:14 +0530

    user login sign up

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,13 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { SignUp } from '../datatype';
+import { login, SignUp } from '../datatype';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private http: HttpClient, private router : Router) {}
+  constructor(private http: HttpClient, private router: Router) {}
   userSignUp(user: SignUp) {
     this.http
       .post('http://localhost:3000/users', user, { observe: 'response' })
@@ -22,5 +22,18 @@ export class UserService {
     if (localStorage.getItem('user')) {
       this.router.navigate(['/']);
     }
+  }
+  userLogIn(user : login) {
+    this.http
+      .get<SignUp[]>(
+        `http://localhost:3000/seller?email=${user.email}&password=${user.password}`,
+        { observe: 'response' }
+      )
+      .subscribe((res) => {
+        if (res && res.body) {
+          localStorage.setItem('user', JSON.stringify(res.body[0]));
+          this.router.navigate(['/']);
+        }
+      });
   }
 }
--- src/app/user-auth/user-auth.component.ts
+++ src/app/user-auth/user-auth.component.ts
@@ -8,12 +8,21 @@ import { UserService } from '../services/user.service';
   styleUrls: ['./user-auth.component.css'],
 })
 export class UserAuthComponent implements OnInit {
-  constructor(private user : UserService) {}
-
+  constructor(private user: UserService) {}
+  isLoggedIn: boolean = false;
   ngOnInit(): void {
     this.user.userAuthReload();
   }
   SignUp(val: SignUp) {
     this.user.userSignUp(val);
+  }
+  LogIn(val: SignUp) {
+    this.user.userLogIn(val);
+  }
+  openLogIn() {
+    this.isLoggedIn = true;
+  }
+  openSignUp() {
+    this.isLoggedIn = false;
   }
 }
