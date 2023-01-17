85780d53125a8476ef8086dc64f576b6ae748a16
SoumyadeepPal21 <soumyadeepbappipal@gmail.com>
2023-01-17 14:45:53 +0530

    done with search page 27

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { product } from '../datatype';
+import { ProductService } from '../services/product.service';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
+  searchResults: undefined | product[];
+  noResultMessage : undefined | string;
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private product: ProductService
+  ) {}
 
   ngOnInit(): void {
+    let query = this.activeRoute.snapshot.paramMap.get('query');
+    query &&
+      this.product.serachProducts(query).subscribe((result) => {
+        if (result.length) {
+          this.searchResults = result;
+        } else {
+          this.noResultMessage = 'No results found';
+        }
+        console.warn(this.noResultMessage);
+      });
   }
-
 }
